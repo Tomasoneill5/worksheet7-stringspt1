74db57272db08aa82f9b71b2de1c0e20b7905136
Tomasoneill5 <92168523+Tomasoneill5@users.noreply.github.com>
2021-11-25 22:39:41 +0000

    Add files via upload

---
+++ string exercise1.py
@@ -0,0 +1,15 @@
+string= 'Hello There Bob'
+print(len(string))
+
+print(string[0])
+print(string[4])
+print(string[-1])
+
+for ch in string:
+    print(ch)
+    
+for ch in range(0,len(string)):
+    print(string[ch])
+
+for ch in range(0,len(string)):
+    print(ch)
---
+++ string exercise2.py
@@ -0,0 +1,15 @@
+string= 'Hello World'
+
+print(string.upper())
+print(string.lower())
+print(string.count('l'))
+print(string.find('l'))
+print(string.replace('l','z'))
+print(string.islower())
+print(string.isupper())
+print(string.upper())
+print(string.isalnum())
+print(string.isalpha())
+print(string.isdigit())
+print(string.index('H'))
+print(string.strip('d'))
